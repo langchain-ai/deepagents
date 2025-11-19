85864ce0e527a109acd2d6de96da109c1e29a6ea
Eugene Yurtsev <eyurtsev@gmail.com>
2025-11-19 14:31:52 -0500

    x

--- libs/harbor/src/harbor_deepagents/agents/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Harbor DeepAgents - Agent implementations."""
-
-from harbor_deepagents.agents.custom_agent import CustomDeepAgent
-from harbor_deepagents.agents.deepagent_harbor import DeepAgentHarbor
-
-__all__ = [
-    "CustomDeepAgent",
-    "DeepAgentHarbor",
-]

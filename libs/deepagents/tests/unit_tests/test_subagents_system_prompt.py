e41309bc72c2eeff66f8688678763c0816d0c771
Junaid Aslam <aslamjunaid838@gmail.com>
2026-01-28 16:06:13 +0500

    lint: fix import grouping

--- libs/deepagents/tests/unit_tests/test_subagents_system_prompt.py
+++ libs/deepagents/tests/unit_tests/test_subagents_system_prompt.py
@@ -3,7 +3,6 @@
 from collections.abc import Callable, Sequence
 
 from langchain.agents import create_agent
-
 from langchain_core.language_models.fake_chat_models import GenericFakeChatModel
 from langchain_core.messages import AIMessage, HumanMessage
 from langchain_core.runnables import Runnable

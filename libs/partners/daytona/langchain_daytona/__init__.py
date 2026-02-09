019c02fbd6b4db46a22b7b61c3823ea59a504038
Eugene Yurtsev <eyurtsev@gmail.com>
2026-02-09 10:09:38 -0500

    x

--- libs/partners/daytona/langchain_daytona/__init__.py
+++ libs/partners/daytona/langchain_daytona/__init__.py
@@ -1,9 +1,7 @@
 """Daytona sandbox integration for Deep Agents."""
 
 from langchain_daytona.sandbox import (
-    DaytonaBackend,
     DaytonaSandbox,
-    DaytonaSandboxClient,
 )
 
-__all__ = ["DaytonaBackend", "DaytonaSandbox", "DaytonaSandboxClient"]
+__all__ = ["DaytonaSandbox"]

7ec67650dea70b9b320f31f95180c958266b07aa
Eugene Yurtsev <eyurtsev@gmail.com>
2026-02-18 11:17:59 -0500

    x

--- libs/partners/daytona/tests/integration_tests/test_integration.py
+++ libs/partners/daytona/tests/integration_tests/test_integration.py
@@ -1,13 +1,17 @@
 from __future__ import annotations
 
-from collections.abc import Iterator
-
-import pytest
-from deepagents.backends.protocol import SandboxBackendProtocol
-from langchain_tests.integration_tests import SandboxIntegrationTests
+from typing import TYPE_CHECKING
 
 import daytona
+import pytest
+from langchain_tests.integration_tests import SandboxIntegrationTests
+
 from langchain_daytona import DaytonaSandbox
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
+
+    from deepagents.backends.protocol import SandboxBackendProtocol
 
 
 class TestDaytonaSandboxStandard(SandboxIntegrationTests):

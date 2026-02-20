888ab0790a2705d1651136695d62014abbc1a733
Mason Daugherty <mason@langchain.dev>
2026-02-20 16:16:32 -0500

    Apply suggestion from @mdrxy

--- libs/deepagents/tests/integration_tests/test_filesystem_middleware.py
+++ libs/deepagents/tests/integration_tests/test_filesystem_middleware.py
@@ -2,7 +2,7 @@ import uuid
 
 import pytest
 from langchain.agents import create_agent
-from langchain.agents.middleware.types import AgentMiddleware
+from langchain.agents.middleware import AgentMiddleware
 from langchain.tools import ToolRuntime
 from langchain_anthropic import ChatAnthropic
 from langchain_core.messages import HumanMessage

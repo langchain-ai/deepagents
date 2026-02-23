78fbd57de6fa459449db4b83c654e3bdabfe52c5
Caleb <1253531891@qq.com>
2026-02-23 16:03:36 +0800

    fix(cli): remove unnecessary async from test

--- libs/cli/tests/unit_tests/test_feedback_choice.py
+++ libs/cli/tests/unit_tests/test_feedback_choice.py
@@ -4,7 +4,7 @@ from textual.pilot import Pilot
 from deepagents_cli.widgets.feedback_choice import FeedbackChoiceScreen
 
 
-async def test_feedback_choice_screen_mounts():
+def test_feedback_choice_screen_mounts():
     """Test that FeedbackChoiceScreen can be instantiated and mounted."""
     app = FeedbackChoiceScreen()
     assert app is not None

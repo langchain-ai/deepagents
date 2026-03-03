5972a014f7db39395511e3d4bacff9b446ec5165
Eugene Yurtsev <eyurtsev@gmail.com>
2026-03-03 11:14:52 -0500

    x

--- libs/deepagents/tests/evals/conftest.py
+++ libs/deepagents/tests/evals/conftest.py
@@ -4,13 +4,15 @@ import pytest
 
 from deepagents.graph import get_default_model
 
+pytest_plugins = ["tests.evals.pytest_reporter"]
+
 
 def pytest_addoption(parser: pytest.Parser) -> None:
     parser.addoption(
         "--model",
         action="append",
         default=None,
-        help=("Model(s) to run evals against. May be provided multiple times. If omitted, uses deepagents.graph.get_default_model().model."),
+        help="Model(s) to run evals against. May be provided multiple times. If omitted, uses deepagents.graph.get_default_model().model.",
     )
 
 

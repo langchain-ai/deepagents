d30d6d000264823a0fc2a6036f00feb393cdd963
Mason Daugherty <mason@langchain.dev>
2026-03-19 02:35:04 -0400

    ci: fix evals dataclass (#2034)

--- libs/deepagents/tests/evals/utils.py
+++ libs/deepagents/tests/evals/utils.py
@@ -73,7 +73,6 @@ class AgentTrajectory:
 # ---------------------------------------------------------------------------
 
 
-@dataclass(frozen=True)
 class SuccessAssertion:
     """Base for correctness assertions that fail the test when violated."""
 

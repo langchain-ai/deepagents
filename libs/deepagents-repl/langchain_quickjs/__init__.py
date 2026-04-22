70e6c9d0b1359e942cb8e3c202380437876614eb
Hunter Lovell <hunter@hntrl.io>
2026-04-22 16:11:28 -0700

    relocate package

--- libs/deepagents-repl/langchain_quickjs/__init__.py
+++
@@ -1,6 +0,0 @@
-"""langchain-quickjs: persistent JS REPL middleware for agents."""
-
-from langchain_quickjs._ptc import PTCConfig, PTCOption
-from langchain_quickjs.middleware import REPLMiddleware
-
-__all__ = ["PTCConfig", "PTCOption", "REPLMiddleware"]

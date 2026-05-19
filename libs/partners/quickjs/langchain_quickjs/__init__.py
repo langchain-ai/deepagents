22a107ec0519a682991bff3b6eaa975b87226246
Colin Francis <colin.francis@langchain.dev>
2026-05-19 11:56:27 -0700

    implement swarm task tool

--- libs/partners/quickjs/langchain_quickjs/__init__.py
+++ libs/partners/quickjs/langchain_quickjs/__init__.py
@@ -1,6 +1,21 @@
 """langchain-quickjs: persistent JS REPL middleware for agents."""
 
 from langchain_quickjs._ptc import PTCOption
+from langchain_quickjs._swarm_task import (
+    SwarmSubAgent,
+    SwarmTaskMode,
+    VariantCache,
+    create_swarm_task_tool,
+    normalize_schema,
+)
 from langchain_quickjs.middleware import CodeInterpreterMiddleware
 
-__all__ = ["CodeInterpreterMiddleware", "PTCOption"]
+__all__ = [
+    "CodeInterpreterMiddleware",
+    "PTCOption",
+    "SwarmSubAgent",
+    "SwarmTaskMode",
+    "VariantCache",
+    "create_swarm_task_tool",
+    "normalize_schema",
+]

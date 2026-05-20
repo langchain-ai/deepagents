a89a257eb288c211cb4c6932b69c73f501ae15ed
Colin Francis <colin.francis@langchain.dev>
2026-05-20 09:43:56 -0700

    extra safety

--- libs/partners/quickjs/langchain_quickjs/_skills.py
+++ libs/partners/quickjs/langchain_quickjs/_skills.py
@@ -253,6 +253,9 @@ def _build_scope_modules(
                 files[rel] = source
         else:
             relocated = rel.removeprefix(strip_prefix) if strip_prefix else rel
+            if relocated.startswith(("/", "../")) or "/../" in relocated:
+                msg = f"skill {skill_name!r}: relocated path {relocated!r} escapes scope"
+                raise SkillInstallError(msg)
             files[relocated] = source
             if relocated != rel:
                 files[rel] = source

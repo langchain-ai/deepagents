71be58f3981af301401d475d758a4a519b9a7a1e
Mason Daugherty <github@mdrxy.com>
2026-06-29 10:55:23 -0400

    re-order

--- libs/code/deepagents_code/widgets/status.py
+++ libs/code/deepagents_code/widgets/status.py
@@ -256,8 +256,8 @@ class StatusBar(Horizontal):
             yield Static("", classes="status-message", id="status-message")
             yield Static("", classes="status-cwd", id="cwd-display")
             yield Static("", classes="status-branch", id="branch-display")
-        yield Static("", classes="status-tokens", id="tokens-display")
         yield Static("", classes="status-rubric", id="rubric-display")
+        yield Static("", classes="status-tokens", id="tokens-display")
         yield ModelLabel(id="model-display")
 
     _BRANCH_WIDTH_THRESHOLD = 100
